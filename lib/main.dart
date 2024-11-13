8927de03efa6379addde44315f1d4471afa9a5b7
Matteo <passematteo@gmail.com>
2024-11-13 19:54:32 +0100

    eate main.dart with MainPage for navigation
    - Created `main.dart` file with `MainPage` to handle navigation across all app pages
    - Set up basic navigation structure for the app

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:radici_parlate/pages/home.dart';
+import 'package:radici_parlate/theme/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Radici Parlate',
+      theme: appTheme,
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int _selectedIndexPage = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          _pages[_selectedIndexPage],
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndexPage = index;
+    });
+  }
+}
