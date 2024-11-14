1d7e0b8969bb221f170903f821efab0a206a2d90
Matteo <passematteo@gmail.com>
2024-11-14 17:20:34 +0100

    Add app strings
    - Added app strings (app nam, app presentation,...)

---
+++ lib/utils/strings.dart
@@ -0,0 +1,4 @@
+class Strings {
+  static const String appName = "Radici Parlate";
+  static const String appPresentation = "Radici Parlate è un'app dedicata alla preservazione dei dialetti e delle tradizioni del Trentino. Scopri, contribuisci e conservali per le generazioni future, mantenendo viva la ricchezza linguistica e culturale della nostra terra.";
+}
