0ef83658e9d914b59fccf6fd71517d3be129f8a5
Matteo <passematteo@gmail.com>
2024-11-24 16:08:31 +0100

    Add genders list and types list
    - Added maschile, femminile, neutro to GenderEnum
    - Added sostantivo, verbo, aggettivo, avverbio, pronome, preposizione, congiunzione, interiezione, articolo to TypeEnum.

--- lib/utils/gender_enum.dart
+++ lib/utils/gender_enum.dart
@@ -2,7 +2,15 @@ class GenderEnum {
   final String value;
   const GenderEnum._(this.value);
 
-  static const List<GenderEnum> values = [];
+  static const maschile = GenderEnum._('maschile');
+  static const femminile = GenderEnum._('femminile');
+  static const neutro = GenderEnum._('neutro');
+
+  static const List<GenderEnum> values = [
+    maschile,
+    femminile,
+    neutro,
+  ];
 
   static GenderEnum fromString(String value) {
     return values.firstWhere((e) => e.value == value);
--- lib/utils/type_enum.dart
+++ lib/utils/type_enum.dart
@@ -2,7 +2,27 @@ class TypeEnum {
   final String value;
   const TypeEnum._(this.value);
 
-  static const List<TypeEnum> values = [];
+  static const sostantivo = TypeEnum._('sostantivo');
+  static const verbo = TypeEnum._('verbo');
+  static const aggettivo = TypeEnum._('aggettivo');
+  static const avverbio = TypeEnum._('avverbio');
+  static const pronome = TypeEnum._('pronome');
+  static const preposizione = TypeEnum._('preposizione');
+  static const congiunzione = TypeEnum._('congiunzione');
+  static const interiezione = TypeEnum._('interiezione');
+  static const articolo = TypeEnum._('articolo');
+
+  static const List<TypeEnum> values = [
+    sostantivo,
+    verbo,
+    aggettivo,
+    avverbio,
+    pronome,
+    preposizione,
+    congiunzione,
+    interiezione,
+    articolo,
+  ];
 
   static TypeEnum fromString(String value) {
     return values.firstWhere((e) => e.value == value);
